7adeb207c96106a26d13dddc1bcc502c4ea07fc5
garyjw <31455599+garyjw@users.noreply.github.com>
2017-12-15 16:44:04 -1000

    Initial Commit

---
+++ core/src/com/wong/game/sprites/Tube.java
@@ -0,0 +1,69 @@
+package com.wong.game.sprites;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.Random;
+
+/**
+ * Created by Gary on 4/4/17.
+ */
+
+public class Tube {
+    public static final int TUBE_WIDTH = 52;
+
+    private static final int FLUCTUATION = 130;
+    private static final int TUBE_GAP = 100;
+    private static final int LOWEST_OPENING = 120;
+
+    private Texture topTube, botTube;
+    private Vector2 posTopTube, posBotTube;
+    private Rectangle boundsTop, boundsBot;
+    private Random rand;
+
+
+    public Tube(float x) {
+        topTube = new Texture("toptube.png");
+        botTube = new Texture("bottomtube.png");
+        rand = new Random();
+
+        posTopTube = new Vector2(x, rand.nextInt(FLUCTUATION) + TUBE_GAP + LOWEST_OPENING);
+        posBotTube = new Vector2(x, posTopTube.y - TUBE_GAP - botTube.getHeight());
+
+        boundsTop = new Rectangle(posTopTube.x, posTopTube.y, topTube.getWidth(), topTube.getHeight());
+        boundsBot = new Rectangle(posBotTube.x, posBotTube.y, botTube.getWidth(), botTube.getHeight());
+    }
+
+    public Texture getTopTube() {
+        return topTube;
+    }
+
+    public Texture getBotTube() {
+        return botTube;
+    }
+
+    public Vector2 getPosTopTube() {
+        return posTopTube;
+    }
+
+    public Vector2 getPosBotTube() {
+        return posBotTube;
+    }
+
+    public void reposition(float x) {
+        posTopTube.set(x, rand.nextInt(FLUCTUATION) + TUBE_GAP + LOWEST_OPENING);
+        posBotTube.set(x, posTopTube.y - TUBE_GAP - botTube.getHeight());
+        boundsTop.setPosition(posTopTube.x, posTopTube.y);
+        boundsBot .setPosition(posBotTube.x, posBotTube.y);
+    }
+
+    public boolean collide(Rectangle player) {
+        return player.overlaps(boundsTop) || player.overlaps(boundsBot);
+    }
+
+    public void dispose() {
+        topTube.dispose();
+        botTube.dispose();
+    }
+}
